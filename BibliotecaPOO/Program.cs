203f6c1ae8e55a1d4252688e8791ff41fb7deae4
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-02-11 17:55:12 -0300

    feat: começando o projeto, iniciando a classe de livros, e emprestimos

---
+++ BibliotecaPOO/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaPOO
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BibliotecaPOO/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaPOO;
+
+public class Users
+{
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    
+}
