468b7d4cd50c2d109b298e198f12fd8aa5ee158b
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-02-12 16:41:37 -0300

    fix: correção de nomes e começo do construtore de usuarios

---
+++ BibliotecaPOO/Entitys/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace BibliotecaPOO;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public DateTime RegisterDate { get; set; }
+    public string Category { get; set; }
+    public int TotalQuantity { get; set; }
+    public int AvailableQuantity { get; set; }
+
+    public bool CanAvailable(int TotalQuantiy, int AvailableQuantity)
+    {
+        if (TotalQuantiy - AvailableQuantity > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false; 
+        }
+    }
+
+    public int ReduceAvailable()
+    {
+        return AvailableQuantity--;
+    }
+
+    public int MoreAvailable()
+    {
+        return AvailableQuantity++;
+    }
+}
