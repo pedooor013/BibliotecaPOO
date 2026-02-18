55654bfde316806f5fdaa2a11012598cdbc572ca
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-02-18 08:00:35 -0300

    feat: criacao das regras de negocio para devolver livros

--- BibliotecaPOO/Services/BibliotecaService.cs
+++ BibliotecaPOO/Services/BibliotecaService.cs
@@ -4,29 +4,48 @@ namespace BibliotecaPOO.Services;
 
 public class BibliotecaService
 {
- public void RealizarEmprestimo(Usuario usuario, Livro livro)
- {
-     if (usuario.StatusUsuario == Usuario.Status.Bloqueado)
-     {
-         Console.WriteLine("O seu status está BLOQUEADO! Não é possível realizar o emprestimo!");
-         return;
-     }
+    public void RealizarEmprestimo(Usuario usuario, Livro livro)
+    {
+        if (usuario.StatusUsuario == Usuario.Status.Bloqueado)
+        {
+            Console.WriteLine("O seu status está BLOQUEADO! Não é possível realizar o emprestimo!");
+            return;
+        }
 
-     if (!livro.Disponivel)
-     {
-         Console.WriteLine("O livro não está disponível para emprestimo!");
-         return;
-     }
+        if (!livro.Disponivel)
+        {
+            Console.WriteLine("O livro não está disponível para emprestimo!");
+            return;
+        }
 
-     if (usuario.Emprestimos.Count > 3)
-     {
-         Console.WriteLine("Limite de emprestimos extrapolado! Não é possível realizar o emprestimo!");
-         return;
-     }
-     Emprestimo emprestimo = new Emprestimo(livro, usuario);
-     
-     usuario.Emprestimos.Add(emprestimo);
- }   
- 
- 
+        if (usuario.Emprestimos.Count > 3)
+        {
+            Console.WriteLine("Limite de emprestimos extrapolado! Não é possível realizar o emprestimo!");
+            return;
+        }
+
+        Emprestimo emprestimo = new Emprestimo(livro, usuario);
+
+        usuario.Emprestimos.Add(emprestimo);
+    }
+
+    public void DevolverLivro(Emprestimo emprestimo)
+    {
+        emprestimo.RegistrarDevolucao();
+
+        bool ehAtrasado = emprestimo.EstaAtrasado();
+
+        if (ehAtrasado)
+        {
+            int diasAtrasados = emprestimo.CalcularDiasAtrasados();
+
+            double valorMultaPorAtraso = diasAtrasados / 0.75;
+            Console.WriteLine(
+                $"O livro está atrasado a {diasAtrasados} dias! Você tera que pagar uma multa de {valorMultaPorAtraso:C}!");
+            emprestimo.Usuario.BloquearUsuario();
+        }
+
+        emprestimo.Livro.DefinirComoNaoEmprestado();
+        
+    }
 }
